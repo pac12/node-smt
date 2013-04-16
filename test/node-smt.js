346a3bae2253533d5cca136acf4e07aa8ba729d5
Nicholas Calugar <njcalugar@gmail.com>
2013-04-16 15:47:17 -0700

    Initial commit

---
+++ test/node-smt.js
@@ -0,0 +1,46 @@
+require("should");
+
+var smt = require('../lib/node-smt.js');
+
+describe('Node SMT', function () {
+  var smtObject;
+
+  before(function () {
+    smtObject = smt.init();
+  });
+
+  describe('Version check', function () {
+    it ('should return 0.0.1', function () {
+      smt.version.should.equal('0.0.1');
+    });
+  });
+
+  describe('Can create a smt object', function () {
+   it ('should return an smt object', function () {
+     smtObject.should.be.a('object');
+     smtObject.should.be.an.instanceOf(Smt);
+   });
+
+   it ('should have a getLeagueList function', function () {
+     smtObject.getLeagueList.should.be.a('function');
+   });
+
+   it ('should have a getTeamIdList function', function () {
+     smtObject.getTeamIdList.should.be.a('function');
+   });
+
+   it ('should have a getRosterSet function', function () {
+     smtObject.getRosterSet.should.be.a('function');
+   });
+
+   it ('should have a getDailySchedule function', function () {
+     smtObject.getDailySchedule.should.be.a('function');
+   });
+
+   it ('should have a getGameScores function', function () {
+     smtObject.getGameScores.should.be.a('function');
+   });
+  });
+
+});
+
