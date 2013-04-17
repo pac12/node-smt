ae01575010f29fdaa129678d3abcde1bf812fdac
Nicholas Calugar <njcalugar@gmail.com>
2013-04-17 16:17:32 -0700

    Finish LeagueList implementation

--- test/league-list.js
+++ test/league-list.js
@@ -27,13 +27,36 @@ describe('LeagueList', function () {
       smtObject = smt.init('127.0.0.1', 40000);
       scope = nock('http://127.0.0.1:40000')
         .get('/GEMWebExchange/GEMWebExchange.svc/sportsmediaxml.lasso?feed=AllSport&type=LeagueList')
-        .replyWithFile(200, __dirname + '/replies/league-list-200.txt');
+        .replyWithFile(200, __dirname + '/replies/league-list-200.txt')
+        .get('/GEMWebExchange/GEMWebExchange.svc/sportsmediaxml.lasso?feed=AllSport&type=LeagueList')
+        .replyWithFile(200, __dirname + '/replies/league-list-bad.txt')
+        .get('/GEMWebExchange/GEMWebExchange.svc/sportsmediaxml.lasso?feed=AllSport&type=LeagueList')
+        .replyWithFile(404, __dirname + '/replies/404.txt');
     });
 
-    it ('should return json league list as result on 200', function (done) {
+    it ('should pass no error and a league list as result on 200', function (done) {
       smtObject.getLeagueList(function (err, result) {
         should.not.exist(err);
-        result.should.match(/BASKETBALL/);
+        result.should.be.a('object');
+        result.LeagueList.should.be.a('object');
+        result.LeagueList.LeagueListEntry.should.be.an.instanceOf(Array);
+        result.LeagueList.LeagueListEntry.should.includeEql({ LeagueId: 'NCAAF', Sport: 'FOOTBALL' });
+        done();
+      });
+    });
+
+    it ('should pass error and no result with bad xml', function (done) {
+      smtObject.getLeagueList(function (err, result) {
+        err.should.match(/Parse error/);
+        should.not.exist(result);
+        done();
+      });
+    });
+
+    it ('should pass error and no result on 404', function (done) {
+      smtObject.getLeagueList(function (err, result) {
+        err.should.match(/HTTP 404/);
+        should.not.exist(result);
         done();
       });
     });
